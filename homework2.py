3a91be19d7019f9aa5a62d796fabcb053363b5e3
Lacey Hamilton <145399024+laceyahamilton@users.noreply.github.com>
2023-09-29 12:35:42 -0400

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,19 +1,19 @@
-streamlit 
-seaborn
-pandas
-plotly.express
+import streamlit as st
+import seaborn as sns
+import pandas as pd
+import plotly.express as px
 
-iris = seaborn.load_dataset('iris')
-streamlit.write("""
+iris = sns.load_dataset('iris')
+st.write("""
 # Iris Dataset 
 """)
-mychart = plotly.express.scatter_3d(iris, x ="petal_length", y="petal_width", z="sepal_width", color="species", color_continuous_scale="greens")
-mychart2 = plotly.express.scatter_3d(iris, x="sepal_length", y="sepal_width", z="petal_width", color="species", color_continuous_scale="greens")
+mychart = px.scatter_3d(iris, x ="petal_length", y="petal_width", z="sepal_width", color="species", color_continuous_scale="greens")
+mychart2 = px.scatter_3d(iris, x="sepal_length", y="sepal_width", z="petal_width", color="species", color_continuous_scale="greens")
 
-tab1, tab2 = streamlit.tabs(["Petal Length vs Petal Width vs Sepal Width", "Sepal Length vs Sepal Width vs Petal Width"])
+tab1, tab2 = st.tabs(["Petal Length vs Petal Width vs Sepal Width", "Sepal Length vs Sepal Width vs Petal Width"])
 
 with tab1:
-    streamlit.plotly_chart(mychart, sharing="streamlit", theme=None)
+    st.plotly_chart(mychart, sharing="streamlit", theme=None)
 
 with tab2:
-    streamlit.plotly_chart(mychart2, sharing="streamlit", theme=None)
+    st.plotly_chart(mychart2, sharing="streamlit", theme=None)
